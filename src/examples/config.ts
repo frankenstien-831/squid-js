3cf578cfae4a232944ea8b18f686c5f444175459
Pedro Gutiérrez <pedrogp9693@gmail.com>
2019-02-08 15:52:25 +0100

    using spree for running examples

--- src/examples/config.ts
+++ src/examples/config.ts
@@ -4,14 +4,12 @@ import * as config from "./config/config.json"
 
 if (process.env.SEED_WORDS) {
     const seedphrase = process.env.SEED_WORDS
-    // There are 20 accounts availabe (19 + 1)
-    const accountIndex = Math.floor(Math.random() * 19)
 
     // @ts-ignore
     config.web3Provider = new HDWalletProvider(
         seedphrase,
         config.nodeUri,
-        accountIndex,
+        0,
         2,
     )
 }
--- src/examples/fire_lazers.sh
+++ src/examples/fire_lazers.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-# this has to go first
+# First of all to log the accounts
+npm run run src/examples/GetAccounts.ts
+
+# Before rest of examples
 npm run run src/examples/RegisterServiceAgreementTemplates.ts
 
+# Examples
 npm run run src/examples/BuyAsset.ts
 npm run run src/examples/ExecuteAgreement.ts
-npm run run src/examples/GetAccounts.ts
 npm run run src/examples/GetBalance.ts
 # npm run run src/examples/GrantAccess.ts
 npm run run src/examples/RegisterAsset.ts
