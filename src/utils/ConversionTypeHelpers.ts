e7b9f990b6084e22f3905ee9999c18bd19f04d49
Pedro Gutiérrez <pedrogp9693@gmail.com>
2019-03-11 22:46:27 +0100

    Add conversion helpers for `did:op:` prefixes.

--- src/utils/ConversionTypeHelpers.ts
+++ src/utils/ConversionTypeHelpers.ts
@@ -1,13 +1,35 @@
 import { Logger } from './Logger'
 
+// Ox transformer
 export const zeroX = (input: string) => zeroXTransformer(input, true)
 export const noZeroX = (input: string) => zeroXTransformer(input, false)
+export function zeroXTransformer(input: string = "", zeroOutput: boolean) {
+    const {valid, output} = inputMatch(input, /^(?:0x)*([a-f0-9]+)$/i, "zeroXTransformer")
+    return (zeroOutput && valid ? "0x" : "") + output
+}
 
-export function zeroXTransformer(input: string, zeroOutput: boolean) {
-    const match = input.match(/^(?:0x)?([a-f0-9]+)$/i)
-    if (!match) {
-        Logger.warn(`Input transformation failed.`)
-        return input
+// did:op: transformer
+export const didPrefixed = (input: string) => didTransformer(input, true)
+export const noDidPrefixed = (input: string) => didTransformer(input, false)
+export function didTransformer(input: string = "", prefixOutput: boolean) {
+    const {valid, output} = inputMatch(input, /^(?:0x|did:op:)*([a-f0-9]{64})$/i, "didTransformer")
+    return (prefixOutput && valid ? "did:op:" : "") + output
+}
+
+// 0x + did:op: transformer
+export const didZeroX = (input: string) => zeroX(didTransformer(input, false))
+
+// Shared functions
+function inputMatch(input: string, regexp: RegExp, conversorName: string): {valid: boolean, output: string} {
+        if (typeof input !== "string") {
+        Logger.debug("Not input string:")
+        Logger.debug(input)
+        throw new Error(`[${conversorName}] Expected string, input type: ${typeof input}`);
     }
-    return (zeroOutput ? "0x" : "") + match[1]
+    const match = input.match(regexp)
+    if (!match) {
+        Logger.warn(`[${conversorName}] Input transformation failed.`)
+        return {valid: false, output: input}
+    }
+    return {valid: true, output: match[1]}
 }
