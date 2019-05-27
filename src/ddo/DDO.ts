7f0d50691c55f6e18142e9f38676281f62edcc45
Pedro Gutiérrez <pedrogp9693@gmail.com>
2019-05-27 19:28:31 +0200

    Use prefix on checksum.

--- src/ddo/DDO.ts
+++ src/ddo/DDO.ts
@@ -102,7 +102,7 @@ export class DDO {
             this.id,
         ]
 
-        return Web3Provider.getWeb3().utils.sha3(values.join("")).replace(/^0x([a-f0-9]{64})(:!.+)?$/i, "$1")
+        return Web3Provider.getWeb3().utils.sha3(values.join("")).replace(/^0x([a-f0-9]{64})(:!.+)?$/i, "0x$1")
     }
 
     /**
